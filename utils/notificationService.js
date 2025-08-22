537a28f0c42014ebd853896e370e97fad473c79a
Davies Precious <preshydave.d5@gmail.com>
2025-08-22 12:40:12 +0100

    project completed

--- utils/notificationService.js
+++ utils/notificationService.js
@@ -3,7 +3,6 @@ import * as Device from "expo-device";
 import { Platform } from "react-native";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { HttpClient } from "../api/HttpClient";
-import Constants from "expo-constants";
 
 // Configure notification behavior
 Notifications.setNotificationHandler({
